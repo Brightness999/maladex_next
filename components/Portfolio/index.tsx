3cec713ce34b994fc1e832c61b59e0b52aa137cc
Brightness999 <brightness0804@gmail.com>
2022-02-14 01:21:01 +0900

    chore: change composition table in portfolio

--- components/Portfolio/index.tsx
+++ components/Portfolio/index.tsx
@@ -13,16 +13,10 @@ type Props = {
 }
 
 const CompositionData = [{
-  platform: '40%',
-  airdrops: '5%',
-  ispo: '5%',
-  year1: '4%',
-  year2: '4%',
-  year3: '4%',
-  year4: '4%',
-  year5: '4%',
-  private: '10%',
-  ifo: '15%',
+  ADA: '25%',
+  MAL: '40%',
+  AGIX: '15%',
+  MELD: '20%',
 }]
 
 const Portfolio: React.FC<Props> = (props) => {
@@ -31,24 +25,12 @@ const Portfolio: React.FC<Props> = (props) => {
       <div className="d-flex p-4">
         <div className={styles.composition}>
           <Composition />
-          <div className='ag-theme-alpine' style={{height: 140}}>
+          <div className='ag-theme-alpine' style={{ height: 100 }}>
             <AgGridReact rowData={CompositionData}>
-              <AgGridColumn headerName="Rewards">
-                <AgGridColumn headerName="Platform Rewards" field='platform' width={150} />
-                <AgGridColumn headerName="Airdrops" field='airdrops' width={90} />
-                <AgGridColumn headerName="ISPO" field='ispo' width={70} />
-              </AgGridColumn>
-              <AgGridColumn headerName="Team">
-                <AgGridColumn headerName="Year1" field='year1' width={80} />
-                <AgGridColumn headerName="Year2" field='year2' width={80} />
-                <AgGridColumn headerName="Year3" field='year3' width={80} />
-                <AgGridColumn headerName="Year4" field='year4' width={80} />
-                <AgGridColumn headerName="Year5" field='year5' width={80} />
-              </AgGridColumn>
-              <AgGridColumn headerName="Sale">
-                <AgGridColumn headerName="Private Sale" field='private' width={150} />
-                <AgGridColumn headerName="IFO" field='ifo' width={80} />
-              </AgGridColumn>
+              <AgGridColumn headerName="ADA" field='ADA' minWidth={80} flex={1} />
+              <AgGridColumn headerName="MAL" field='MAL' minWidth={80} flex={1} />
+              <AgGridColumn headerName="AGIX" field='AGIX' minWidth={80} flex={1} />
+              <AgGridColumn headerName="MELD" field='MELD' minWidth={80} flex={1} />
             </AgGridReact>
           </div>
         </div>
