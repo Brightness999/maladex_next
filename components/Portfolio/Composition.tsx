14264fbaa9a54bdac6dd9f50c2ca63c86b219989
Brightness999 <brightness0804@gmail.com>
2022-02-14 01:11:45 +0900

    chore: change the composition chart in portfolio

--- components/Portfolio/Composition.tsx
+++ components/Portfolio/Composition.tsx
@@ -5,72 +5,16 @@ import HighchartsReact from 'highcharts-react-official';
 const Composition = (props: HighchartsReact.Props) => {
   const [labelsize, setLabelSize] = useState<string>('');
   const [labeldistance, setLabelDistance] = useState<number>(0);
-  const [detaillabelsize, setDetailLabelSize] = useState<string>('');
-  const [detaillabeldistance, setDetailLabelDistance] = useState<number>(0);
   const [height, setHeight] = useState<number>(0);
   const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
-  const LightenDarkenColor = (col, amt) => {
-    let usePound = false;
-    if (col[0] == "#") {
-      col = col.slice(1);
-      usePound = true;
-    }
-    let num = parseInt(col, 16);
-    let r = (num >> 16) * (1 + amt);
-    if (r > 255) r = 255;
-    else if (r < 0) r = 0;
-    let b = ((num >> 8) & 0x00FF) * (1 + amt);
-    if (b > 255) b = 255;
-    else if (b < 0) b = 0;
-    let g = (num & 0x0000FF) * (1 + amt);
-    if (g > 255) g = 255;
-    else if (g < 0) g = 0;
-    return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
-  }
-  const categories = ['Rewards', 'Team', 'Sale'];
+  const categories = ['ADA', 'MAL', 'AGIX', 'MELD'];
   const data = [
-    {
-      y: 50,
-      color: '#e91919',
-      drilldown: {
-        name: 'Rewards',
-        categories: [
-          'Platform: 40%',
-          'Airdrops: 5%',
-          'ISPO: 5%'
-        ],
-        data: [40, 5, 5]
-      }
-    },
-    {
-      y: 20,
-      color: '#baff23',
-      drilldown: {
-        name: 'Team',
-        categories: [
-          'Y1: 4%',
-          'Y2: 4%',
-          'Y3: 4%',
-          'Y4: 4%',
-          'Y5: 4%'
-        ],
-        data: [4, 4, 4, 4, 4]
-      }
-    },
-    {
-      y: 30,
-      color: '#19aac6',
-      drilldown: {
-        name: 'Sale',
-        categories: [
-          'Private: 5%',
-          'IFO: 25%'
-        ],
-        data: [5, 25]
-      }
-    }
+    { y: 25, color: '#e91919' },
+    { y: 40, color: '#baff23' },
+    { y: 15, color: '#19aac6' },
+    { y: 20, color: '#8085e9' }
   ];
-  let distData = [], distDetailData = [], drillDataLen: number, brightness: number;
+  let distData = [];
 
   for (let i = 0; i < data.length; i++) {
     distData.push({
@@ -78,17 +22,7 @@ const Composition = (props: HighchartsReact.Props) => {
       y: data[i].y,
       color: data[i].color
     });
-    drillDataLen = data[i].drilldown.data.length;
-    for (let j = 0; j < drillDataLen; j++) {
-      brightness = 0.2 - (j / drillDataLen) / 5;
-      distDetailData.push({
-        name: data[i].drilldown.categories[j],
-        y: data[i].drilldown.data[j],
-        color: LightenDarkenColor(data[i].color, brightness)
-      });
-    }
   }
-
 
   const options: Highcharts.Options = {
     chart: {
@@ -105,7 +39,7 @@ const Composition = (props: HighchartsReact.Props) => {
       pie: {
         shadow: false,
         center: ['50%', '50%'],
-        startAngle: 180,
+        startAngle: 195,
       }
     },
     tooltip: {
@@ -115,7 +49,7 @@ const Composition = (props: HighchartsReact.Props) => {
       type: 'pie',
       name: 'Allocation',
       data: distData,
-      size: '60%',
+      size: '100%',
       dataLabels: {
         formatter: function () {
           return this.y > 5 ? this.point.name : null;
@@ -126,25 +60,6 @@ const Composition = (props: HighchartsReact.Props) => {
           fontSize: labelsize
         }
       }
-    }, {
-      type: 'pie',
-      name: 'Allocation',
-      data: distDetailData,
-      size: '100%',
-      innerSize: '60%',
-      dataLabels: {
-        formatter: function () {
-          return this.y > 1 ? this.point.name.split(':')[0] : null;
-        },
-        crop: false,
-        color: '#ffffff',
-        connectorWidth: 0,
-        distance: detaillabeldistance,
-        style: {
-          fontSize: detaillabelsize,
-        }
-      },
-      id: 'percentages'
     }],
     responsive: {
       rules: [{
@@ -166,24 +81,22 @@ const Composition = (props: HighchartsReact.Props) => {
     }
   }
 
-  const setChartDimensions = (ls: string, dls: string, ld: number, dld: number, h: number) => {
+  const setChartDimensions = (ls: string, ld: number, h: number) => {
     setLabelSize(ls);
-    setDetailLabelSize(dls);
     setLabelDistance(ld);
-    setDetailLabelDistance(dld);
     setHeight(h);
   }
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       if (window.innerWidth > 1440) {
-        setChartDimensions('20px', '16px', -68, -40, 500);
+        setChartDimensions('20px', -68, 500);
       } else if (window.innerWidth > 992) {
-        setChartDimensions('14px', '12px', -50, -40, 400);
+        setChartDimensions('14px', -50, 400);
       } else if (window.innerWidth > 450) {
-        setChartDimensions('20px', '16px', -68, -40, 500);
+        setChartDimensions('20px', -68, 500);
       } else {
-        setChartDimensions('14px', '12px', -40, -25, 300);
+        setChartDimensions('14px', -40, 300);
       }
     }
   }, []);
