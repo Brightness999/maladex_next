aad76c90ad5d316e9f61ba0e241bb3144ff0aa64
Brightness999 <brightness0804@gmail.com>
2022-02-23 06:24:52 +0900

    chore: add useeffect in diagrams

--- pages/mint/programmable-swap/Diagrams.tsx
+++ pages/mint/programmable-swap/Diagrams.tsx
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { Application } from './Application';
 
 const BodyWidget = dynamic(() => import('./components/BodyWidget'), { ssr: false });
 
 const Diagrams: React.FC = () => {
-  var newapp = new Application;
   const [isadd, setIsAdd] = useState<boolean>(false);
-  const [app, setApp] = useState<Application>(newapp);
+  const [app, setApp] = useState<Application>(null);
+
   const handleAdd = (status: boolean) => {
     setIsAdd(status);
   }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      var newapp = new Application;
+      setApp(newapp);
+    }
+  }, []);
+  
   return <BodyWidget app={app} isadd={isadd} handleAdd={(status: boolean) => handleAdd(status)} />;
 }
 
