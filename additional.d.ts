fb348f82912e07370fe12ce3e6bfd048333ab6ee
Brightness999 <brightness0804@gmail.com>
2022-03-09 03:14:32 +0900

    feat: add gerowallet connection

--- additional.d.ts
+++ additional.d.ts
@@ -12,5 +12,27 @@ interface Window {
       enable?: (...args: any[]) => Promise<any>
       isEnabled?: (...args: any[]) => Promise<boolean>
     }
+    gerowallet?: {
+      enable?: (...args: any[]) => Promise<any>
+      experimental?: {
+        on?: (...args: any[]) => Promise<any>
+        off?: (...args: any[]) => Promise<any>
+        getCollateral?: (...args: any[]) => Promise<any>
+      }
+      getBalance?: (...args: any[]) => Promise<any>
+      getChangeAddress?: (...args: any[]) => Promise<any>
+      getCollateral?: (...args: any[]) => Promise<any>
+      getNetworkId?: (...args: any[]) => Promise<any>
+      getRewardAddresses?: (...args: any[]) => Promise<any>
+      getUnusedAddresses?: (...args: any[]) => Promise<any>
+      getUsedAddresses?: (...args: any[]) => Promise<any>
+      getUtxos?: (...args: any[]) => Promise<any>
+      isEnabled?: (...args: any[]) => Promise<boolean>
+      onAccountChange?: (...args: any[]) => Promise<any>
+      onNetworkChange?: (...args: any[]) => Promise<any>
+      signData?: (...args: any[]) => Promise<any>
+      signTx?: (...args: any[]) => Promise<any>
+      submitTx?: (...args: any[]) => Promise<any>
+    }
   }
 }
