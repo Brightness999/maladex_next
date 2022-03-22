687f269578aabf3e4a36c75e3595ca36adf687b9
Brightness999 <brightness0804@gmail.com>
2022-03-23 07:51:15 +0900

    chore: change node style

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const path = require('path')
-const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  /* bydefault config  option Read For More Optios
-here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
-  cssModules: true
-})
 
 module.exports = {
   sassOptions: {
